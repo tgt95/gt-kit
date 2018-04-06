06a3ff564ae952d3c8b95dc3525cd6b6c32306bb
Gia Tuan <tuan.trinh@bigin.vn>
2018-04-06 10:06:25 +0700

    Update Page

--- demo/index.php
+++ demo/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -8,11 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="demo/assets/img/favicon.ico">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
     <!-- App css -->
-    <link href="built/css/core.css" rel="stylesheet" type="text/css" />
-    <link href="built/css/gt-kit.min.css" rel="stylesheet" type="text/css" />
-    <link href="built/css/demo.min.css" rel="stylesheet" type="text/css" />
+    <link href="../built/css/core.css" rel="stylesheet" type="text/css" />
+    <link href="../built/css/gt-kit.min.css" rel="stylesheet" type="text/css" />
+    <link href="../built/css/demo.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -20,9 +19,9 @@
     <div id="wrapper">
         <div class="content-page">
             <div class="page-header">
-                <div class="page-header-image bg-size-cover" layer-bg="black" style="background: url(demo/assets/img/bg/rodion-kutsaev-951-unsplash.jpg);">
+                <div class="page-header-image bg-size-cover" layer-bg="black" style="background: url(assets/img/bg/rodion-kutsaev-951-unsplash.jpg);">
                     <div class="content-center">
-                        <img class="logo" src="demo/assets/img/logo.svg" height="150">
+                        <img class="logo" src="assets/img/logo.svg" height="150">
                         <h1 class="title">GT UI KIT</h1>
                         <h3 class="descripton">A beautiful Bootstrap 4 UI kit.</h3>
                         <h6 class="sub-descripton">Base on Material Design</h6>
@@ -87,7 +86,14 @@
                         <h4 class="group-title">Background</h4>
                         <p class="category">Pick your style</p>
                         <div class="row">
-                        <div class='col-12 py-2 bg-custom'><code class='bg-white p-1'>bg-custom</code></div><div class='col-12 py-2 bg-primary'><code class='bg-white p-1'>bg-primary</code></div><div class='col-12 py-2 bg-secondary'><code class='bg-white p-1'>bg-secondary</code></div><div class='col-12 py-2 bg-blue'><code class='bg-white p-1'>bg-blue</code></div><div class='col-12 py-2 bg-indigo'><code class='bg-white p-1'>bg-indigo</code></div><div class='col-12 py-2 bg-purple'><code class='bg-white p-1'>bg-purple</code></div><div class='col-12 py-2 bg-pink'><code class='bg-white p-1'>bg-pink</code></div><div class='col-12 py-2 bg-red'><code class='bg-white p-1'>bg-red</code></div><div class='col-12 py-2 bg-orange'><code class='bg-white p-1'>bg-orange</code></div><div class='col-12 py-2 bg-yellow'><code class='bg-white p-1'>bg-yellow</code></div><div class='col-12 py-2 bg-green'><code class='bg-white p-1'>bg-green</code></div><div class='col-12 py-2 bg-teal'><code class='bg-white p-1'>bg-teal</code></div><div class='col-12 py-2 bg-cyan'><code class='bg-white p-1'>bg-cyan</code></div><div class='col-12 py-2 bg-white'><code class='bg-white p-1'>bg-white</code></div><div class='col-12 py-2 bg-black'><code class='bg-white p-1'>bg-black</code></div><div class='col-12 py-2 bg-gray'><code class='bg-white p-1'>bg-gray</code></div><div class='col-12 py-2 bg-gray-dark'><code class='bg-white p-1'>bg-gray-dark</code></div><div class='col-12 py-2 bg-success'><code class='bg-white p-1'>bg-success</code></div><div class='col-12 py-2 bg-info'><code class='bg-white p-1'>bg-info</code></div><div class='col-12 py-2 bg-warning'><code class='bg-white p-1'>bg-warning</code></div><div class='col-12 py-2 bg-danger'><code class='bg-white p-1'>bg-danger</code></div><div class='col-12 py-2 bg-light'><code class='bg-white p-1'>bg-light</code></div><div class='col-12 py-2 bg-dark'><code class='bg-white p-1'>bg-dark</code></div>                        </div>
+                        <?php
+                        $baseColor = array('custom', 'primary', 'secondary', 'blue', 'indigo', 'purple', 'pink', 'red', 'orange', 'yellow', 'green', 'teal', 'cyan', 'white', 'black', 'gray', 'gray-dark', 'success', 'info', 'warning', 'danger', 'light', 'dark');
+
+                        foreach ($baseColor as $value) {
+                            echo "<div class='col-12 py-2 bg-". $value . "'><code class='bg-white p-1'>bg-". $value ."</code></div>";
+                        } 
+                        ?>
+                        </div>
                     </div>
                 </div>
                 <!-- container -->
@@ -106,7 +112,7 @@
     <!-- END wrapper -->
 
 </body>
-<script src="built/js/core.min.js"></script>
+<script src="../built/js/core.min.js"></script>
 <script>
 $(document).ready(function() {
     var windowHeight = window.innerHeight;
