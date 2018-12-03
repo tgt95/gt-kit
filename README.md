0dbd4ef0fdbf473e0a1b3b92c926148e84a1e8de
Gia Tuấn Trình <giatuantrinh@gmail.com>
2018-12-03 07:45:21 +0700

    Update Active Menu Class CSS

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Bootstrap UI Kit base on Material Design 
 
 Demo: https://tgt95.github.io/gt-kit/
+
+Compile SCSS:
+1. Gulp: 
+	- Compile & watch: npm run dev
--- demo/includes/common/_sidebar.php
+++ demo/includes/common/_sidebar.php
@@ -1,5 +1,6 @@
 <aside class="sidebar sidebar-default">
     <div class="side-nav">
+        <!-- Header -->
         <div class="sidebar-header" style="background-image: url(https://2.bp.blogspot.com/-2RewSLZUzRg/U-9o6SD4M6I/AAAAAAAADIE/voax99AbRx0/s1600/14%2B-%2B1%2B%281%29.jpg);">
             <div class="user-thumbnail">
                 <img src="https://scontent.fsgn5-4.fna.fbcdn.net/v/t1.0-1/p320x320/44725791_2217125071685760_7187074006837624832_n.jpg?_nc_cat=102&_nc_ht=scontent.fsgn5-4.fna&oh=77f584b1e393f02a1e310a48af05965a&oe=5C4378B3">
@@ -11,10 +12,14 @@
                 </a>
             </div>
         </div>
+
+        <!-- Footer  -->
         <div class="sidebar-footer">
             <p class="copyright">GT UI KIT © </p>
             <p class="mb-0"><script>document.write(new Date().getFullYear())</script></p>
         </div>
+
+        <!-- User Info -->
         <div class="sidebar-user-info">
             <ul>
                 <?php 
@@ -29,6 +34,8 @@
                 <?php } ?>
             </ul>
         </div>
+
+        <!-- Menu -->
         <div class="sidebar-menu">
             <ul>
                 <li>
@@ -52,32 +59,33 @@
                             <ul>
                             </ul>
                         </ul>
-                    </li>
-                    <li class="has-children">
-                        <a href="#">
-                            <i class="zmdi zmdi-sort-amount-desc"></i> Levels 
-                        </a>
-                        <ul class="submenu">
-                            <li class="has-children">
-                                <a href="#">Level 1.1 </a>
-                                <ul class="submenu">
-                                    <?php for ($i=1; $i < 3; $i++) { ?>
-                                        <li>
-                                            <a href="#">Level 2.<?php echo $i; ?> </a>
-                                        </li>
-                                    <?php } ?>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Level 1.2 </a>
-                            </li>
-                            <li>
-                                <a href="#">Level 1.3 </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
+                    </a>
+                </li>
+                <li class="has-children">
+                    <a href="#">
+                        <i class="zmdi zmdi-sort-amount-desc"></i> Levels 
+                    </a>
+                    <ul class="submenu">
+                        <li class="has-children">
+                            <a href="#">Level 1.1 </a>
+                            <ul class="submenu">
+                                <?php for ($i=1; $i < 3; $i++) { ?>
+                                    <li>
+                                        <a href="#">Level 2.<?php echo $i; ?> </a>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Level 1.2 </a>
+                        </li>
+                        <li>
+                            <a href="#">Level 1.3 </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
+</div>
 </aside>
